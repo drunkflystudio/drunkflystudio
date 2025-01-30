a59c4f82e999b20eaad44fe6613e8cdf594bd609
Sergey Ryabchenkov(freevm) <ihtiandr9@yandex.ru>
2025-01-30 09:36:07 +0800

    fix os detection in linux_install_deps.sh

--- _tools/linux_install_deps.sh
+++ _tools/linux_install_deps.sh
@@ -1,27 +1,33 @@
 #!/bin/sh
 install=
+distrib=$(grep -w ID /etc/os-release | cut -d "=" -f 2)
 
-dpkg -l | grep gcc-multilib >/dev/null 2>&1
-if [ "$?" != 0 ]; then
+if [ "$distrib" = "altlinux" ]; then
+    echo Distributive  ${distrib} have no gcc-multilib pkg
+    echo you have to downoad it manually
+else
+    dpkg -l | grep gcc-multilib >/dev/null 2>&1
+    if [ "$?" != 0 ]; then
     install="$install gcc-multilib"
-fi
+    fi
 
-dpkg -l | grep g++-multilib >/dev/null 2>&1
-if [ "$?" != 0 ]; then
-    install="$install g++-multilib"
-fi
+    dpkg -l | grep g++-multilib >/dev/null 2>&1
+    if [ "$?" != 0 ]; then
+        install="$install g++-multilib"
+    fi
 
-which cmake >/dev/null 2>&1
-if [ "$?" != 0 ]; then
-    install="$install cmake"
-fi
+    which cmake >/dev/null 2>&1
+    if [ "$?" != 0 ]; then
+        install="$install cmake"
+    fi
 
-which ninja >/dev/null 2>&1
-if [ "$?" != 0 ]; then
-    install="$install ninja-build"
-fi
+    which ninja >/dev/null 2>&1
+    if [ "$?" != 0 ]; then
+        install="$install ninja-build"
+    fi
 
-if [ "$install" != "" ]; then
-    echo Need to install the following packages: $install
-    sudo apt-get install $install
+    if [ "$install" != "" ]; then
+        echo Need to install the following packages: $install
+        sudo apt-get install $install
+    fi
 fi
