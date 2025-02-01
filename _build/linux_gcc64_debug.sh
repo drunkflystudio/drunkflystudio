a0173563c1e16791d064a47775917eb7c1b85ef6
Drunk Fly Team <drunkflyteam@gmail.com>
2025-02-01 21:44:28 +0200

    Set CMAKE_HOST_SYSTEM_NAME for buildtool when building on Linux.

--- _build/linux_gcc64_debug.sh
+++ _build/linux_gcc64_debug.sh
@@ -3,5 +3,5 @@ set -e
 dir="$(dirname -- "$(readlink -f -- "$0")")" #"
 "$dir/../_tools/linux_install_deps.sh"
 cd "$dir"
-cmake -DLINUX64=debug -P "$dir/../_buildtool/buildtool.cmake"
+cmake -DCMAKE_HOST_SYSTEM_NAME=Linux -DLINUX64=debug -P "$dir/../_buildtool/buildtool.cmake"
 echo == SUCCESS! ==
