d450cecced12e9b1ae19d2f7bcdbd0512120697a
Drunk Fly Team <drunkflyteam@gmail.com>
2025-02-09 16:46:20 +0200

    Update submodules before CLOC and fix destination branch.

--- _tools/count_lines_of_code.sh
+++ _tools/count_lines_of_code.sh
@@ -2,6 +2,7 @@
 set -e
 cd $(dirname "$0")
 cd ..
+git submodule update --init --recursive
 perl _tools/cloc/cloc-2.04.pl --config=_tools/cloc/cloc.cfg --md --out=_build/CLOC.md .
 lines=`perl _tools/cloc/cloc-2.04.pl --config=_tools/cloc/cloc.cfg --csv . | grep SUM | awk -F, '{OFS=",";print $5;}'`
 wget "https://img.shields.io/badge/$lines-green?style=flat" -O _build/CLOC.svg
