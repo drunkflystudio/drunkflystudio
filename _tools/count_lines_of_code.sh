c8be909e0e210a10a6a26256be21fb7f23f261f0
Drunk Fly Team <drunkflyteam@gmail.com>
2025-02-09 16:43:34 +0200

    Fixes for the scripts.

--- _tools/count_lines_of_code.sh
+++ _tools/count_lines_of_code.sh
@@ -2,6 +2,6 @@
 set -e
 cd $(dirname "$0")
 cd ..
-perl _tools\cloc\cloc-2.04.pl --config=_tools\cloc\cloc.cfg --md --out=_build/CLOC.md .
-lines=`perl _tools\cloc\cloc-2.04.pl --config=_tools\cloc\cloc.cfg --csv . | grep SUM | awk -F, '{OFS=",";print $5;}'`
+perl _tools/cloc/cloc-2.04.pl --config=_tools/cloc/cloc.cfg --md --out=_build/CLOC.md .
+lines=`perl _tools/cloc/cloc-2.04.pl --config=_tools/cloc/cloc.cfg --csv . | grep SUM | awk -F, '{OFS=",";print $5;}'`
 wget "https://img.shields.io/badge/$lines-green?style=flat" -O _build/CLOC.svg
