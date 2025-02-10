08d2d8a8acea38ffba61f0de534b8a728393ba35
Drunk Fly Team <drunkflyteam@gmail.com>
2025-02-10 12:36:34 +0200

    Improvement for all_push.cmd/all_pull.cmd.

--- all_pull.cmd
+++ all_pull.cmd
@@ -1,6 +1,12 @@
 @echo off
+
+echo ========= Pulling base-common...
 git pull || exit /B 1
 cd /D "%~dp0base-common" || exit /B 1
+
+echo ========= Pulling base-compiler...
 git pull || exit /B 1
 cd /D "%~dp0base-compiler" || exit /B 1
+
+echo ========= Pulling main repo...
 git pull || exit /B 1
--- all_push.cmd
+++ all_push.cmd
@@ -1,6 +1,13 @@
 @echo off
-git push || exit /B 1
+
+echo ========= Pushing base-common...
 cd /D "%~dp0base-common" || exit /B 1
 git push || exit /B 1
+
+echo ========= Pushing base-compiler...
 cd /D "%~dp0base-compiler" || exit /B 1
+git push || exit /B 1
+
+echo ========= Pushing main repo...
+cd /D "%~dp0" || exit /B 1
 git push || exit /B 1
